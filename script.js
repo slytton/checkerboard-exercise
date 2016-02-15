e14d504601bb9bc0f1f64878cdc65884bdc2bebd
Seth Lytton <slytton@gmail.com>
2016-02-15 15:43:51 -0700

    complete flashing portion of the exercise.

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 // Your JS goes here
 
 var body = document.getElementsByTagName('body')[0];
-var board = document.createElement('main');
+var board = null;
 var boardWidth = 9;
 
-body.style.margin = "0";
-board.className = 'board';
-body.insertBefore(board, body.firstChild);
+setColors();
+window.setInterval(setColors, 2000);
 
-for (var i = 1; i <= Math.pow(9,2); i++) {
-  var color1 = Math.floor(Math.random() * 255 + 1);
-  var color2 = Math.floor(Math.random() * 255 + 1);
-  var color3 = Math.floor(Math.random() * 255 + 1);
-  var tile = document.createElement('div');
-  tile.style.width = '11.1vw';
-  tile.style.height = '11.1vw';
-  tile.style.float = 'left';
-  tile.style.backgroundColor = 'rgb(' + color1 + ',' + color2 + ',' + color3 + ')'
-  board.appendChild(tile);
+function setColors(){
+  if(board) board.remove()
+  board = document.createElement('main');
+  body.style.margin = "0";
+  board.className = 'board';
+  body.insertBefore(board, body.firstChild);
+
+  for (var i = 1; i <= Math.pow(9,2); i++) {
+    var color1 = Math.floor(Math.random() * 255 + 1);
+    var color2 = Math.floor(Math.random() * 255 + 1);
+    var color3 = Math.floor(Math.random() * 255 + 1);
+    var tile = document.createElement('div');
+    tile.style.width = '11.1vw';
+    tile.style.height = '11.1vw';
+    tile.style.float = 'left';
+    tile.style.backgroundColor = 'rgb(' + color1 + ',' + color2 + ',' + color3 + ')'
+    board.appendChild(tile);
+  }
 }
