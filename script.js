664267c9c8ca4b101f685b9302dea1c4f5da1277
Seth Lytton <slytton@gmail.com>
2016-02-15 15:18:16 -0700

    Complete the gradient assignment

--- script.js
+++ script.js
@@ -3,20 +3,24 @@
 var body = document.getElementsByTagName('body')[0];
 var board = document.createElement('main');
 var boardWidth = 9;
+var color = 59;
 
 body.style.margin = "0";
 board.className = 'board';
 body.insertBefore(board, body.firstChild);
 
-for (var i = 1; i <= Math.pow(9,2); i++) {
-  var tile = document.createElement('div');
-  tile.style.width = '11.1vw';
-  tile.style.height = '11.1vw';
-  tile.style.float = 'left';
-  if(i % 2 === 0){
-    tile.style.backgroundColor = 'red';
-  }else{
-    tile.style.backgroundColor = 'black';
+for (var i = 0; i < 9; i++) {
+  for (var j = 0; j < 9; j++) {
+    var tile = document.createElement('div');
+    tile.style.width = '11.1vw';
+    tile.style.height = '11.1vw';
+    tile.style.float = 'left';
+    if((j + i * 9)  % 2 === 0){
+      tile.style.backgroundColor = 'rgb(59,' + color + ",238)";
+    }else{
+      tile.style.backgroundColor = 'rgb(' + color + ',40' + ",238)";;
+    }
+    board.appendChild(tile);
   }
-  board.appendChild(tile);
+  color += Math.floor(141 / 9);
 }
