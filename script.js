80c8fea9f4ab379752d6e6913de51b197de33bff
Seth Lytton <slytton@gmail.com>
2016-02-15 14:56:51 -0700

    Create checkerboard using only js.

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // Your JS goes here
+
+var body = document.getElementsByTagName('body')[0];
+var board = document.createElement('main');
+var boardWidth = 9;
+
+body.style.margin = "0";
+board.className = 'board';
+body.insertBefore(board, body.firstChild);
+
+for (var i = 1; i <= Math.pow(9,2); i++) {
+  var tile = document.createElement('div');
+  tile.style.width = '11.1vw';
+  tile.style.height = '11.1vw';
+  tile.style.float = 'left';
+  if(i % 2 === 0){
+    tile.style.backgroundColor = 'red';
+  }else{
+    tile.style.backgroundColor = 'black';
+  }
+  board.appendChild(tile);
+}
